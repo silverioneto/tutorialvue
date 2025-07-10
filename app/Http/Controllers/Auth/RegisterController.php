d6d43a56a74bca8bc6eaa207a5c0c48067303818
Silvério Neto <netores1@gmail.com>
2025-07-10 15:55:01 -0300

    feat: implement user registration functionality with validation and error handling

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,14 +2,34 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function create()
     {
         return Inertia::render('Auth/Register');
+    }
+
+    public function store(Request $request)
+    {
+        
+
+        $credentials = $request->validate([
+                'name' => 'required|max:255',
+                'email' => 'required|lowercase|email|max:124',
+                'password' => 'required|confirmed|min:3'
+        ]);
+
+        $user = User::create($credentials);
+
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
 }
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -4,6 +4,22 @@ import Title from "../../Components/Title.vue";
 import TextLink from "../../Components/TextLink.vue";
 import InputField from "../../Components/InputField.vue";
 import PrimaryBtn from "../../Components/PrimaryBtn.vue";
+import ErrorMessages from "../../Components/ErrorMessages.vue";
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+})
+
+const submit = () => {
+    form.post(route('register'), {
+        onFinish: () => form.reset('password', 'password_confirmation')
+    })
+}
+
 </script>
 
 <template>
@@ -16,21 +32,24 @@ import PrimaryBtn from "../../Components/PrimaryBtn.vue";
             </p>
         </div>
 
-        <form class="space-y-6">
-            <InputField label="Name" icon="id-badge" />
+        <!-- Errors Messages -->
+         <ErrorMessages :errors="form.errors"/>
 
-            <InputField label="Email" type="email" icon="at" />
+        <form @submit.prevent="submit" class="space-y-6">
+            <InputField label="Name" icon="id-badge" v-model="form.name" />
 
-            <InputField label="Password" type="password" icon="key" />
+            <InputField label="Email" type="email" icon="at" v-model="form.email" />
 
-            <InputField label="Confirm Password" type="password" icon="key" />
+            <InputField label="Password" type="password" icon="key" v-model="form.password" />
+
+            <InputField label="Confirm Password" type="password" icon="key" v-model="form.password_confirmation" />
 
             <p class="text-slate-500 text-sm dark:text-slate-400">
                 By creating an account, you agree to our Terms of Service and
                 Privacy Policy.
             </p>
 
-            <PrimaryBtn>Register</PrimaryBtn>
+            <PrimaryBtn :disabled="form.processing">Register</PrimaryBtn>
         </form>
     </Container>
 </template>
